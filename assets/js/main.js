b9afc73cf5aaf777a4f731750a05d66675e28ec0
nicholasestrada <nicholas26cervatti@gmail.com>
2023-09-20 14:39:14 -0300

    aplicado a modal com as informações mais detalhadas na modal

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,21 @@
 const pokemonList = document.getElementById('pokemonList')
 const loadMoreButton = document.getElementById('loadMoreButton')
 
+const modal = document.getElementById('pokemonModal');
+
+modal.style.display = 'none';
+
+function fecharModal() {
+    const modal = document.getElementById('pokemonModal');
+    modal.style.display = 'none';
+}
+
+// Adicione um evento de clique ao botão "x" para fechar a modal
+const closeButton = document.querySelector('.close');
+if (closeButton) {
+    closeButton.addEventListener('click', fecharModal);
+}
+
 const maxRecords = 151
 const limit = 10
 let offset = 0;
@@ -18,6 +33,8 @@ function convertPokemonToLi(pokemon) {
 
                 <img src="${pokemon.photo}"
                      alt="${pokemon.name}">
+
+                     
             </div>
         </li>
     `
@@ -45,3 +62,51 @@ loadMoreButton.addEventListener('click', () => {
         loadPokemonItens(offset, limit)
     }
 })
+
+function exibirDetalhesNaModal(pokemon) {
+    const modal = document.getElementById('pokemonModal');
+    const modalContent = modal.querySelector('.modal-content');
+    const modalPokemonDetails = document.getElementById('modalPokemonDetails');
+
+    // Construa o HTML dos detalhes com base nos dados do Pokémon
+    const html = `
+        <div class="poke ${pokemon.type}">
+        <div class="nomePokemon">
+            <h3>${pokemon.name}</h3>
+        </div>
+        <div class="infosPokemon">
+            <p>Número: #${pokemon.number}</p>
+            <p>Tipo: ${pokemon.type}</p>
+            <p>Habilidades: ${pokemon.types.join(', ')}</p>
+        </div>
+            <img id="img" src="${pokemon.photo}"
+                     alt="${pokemon.name}">
+        </div>
+    `;
+
+    // Defina o HTML dos detalhes na modal
+    modalPokemonDetails.innerHTML = html;
+
+    // Exiba a modal
+    modal.style.display = 'block';
+}
+
+
+pokemonList.addEventListener('click', (event) => {
+    const clickedPokemon = event.target.closest('.pokemon');
+    if (clickedPokemon) {
+
+        const numberElement = clickedPokemon.querySelector('.number');
+
+        const pokemonNumber = numberElement.textContent.replace('#', '')
+
+        // Certifique-se de que você tenha um objeto pokemon com uma propriedade 'url'
+        const pokemon = {
+            url: `https://pokeapi.co/api/v2/pokemon/${pokemonNumber}/`
+        };
+
+        pokeApi.getPokemonDetail(pokemon).then((details) => {
+            exibirDetalhesNaModal(details);
+        });
+    }
+});
